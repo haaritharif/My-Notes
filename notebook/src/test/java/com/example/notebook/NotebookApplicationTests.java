ff3c6bf2b6f0bf32d16add96afc0380c76cdc89e
Haarith Vohra <haarithvohra@gmail.com>
2022-06-02 01:43:08 -0400

    adding tests

--- notebook/src/test/java/com/example/notebook/NotebookApplicationTests.java
+++ notebook/src/test/java/com/example/notebook/NotebookApplicationTests.java
@@ -1,14 +1,35 @@
 package com.example.notebook;
 
+import com.example.notebook.Controller.NoteController;
+import com.example.notebook.DTO.Note;
 import org.junit.jupiter.api.Test;
+import org.mockito.stubbing.Answer;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.mo
+import reactor.core.publisher.Mono;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
+import static org.junit.Assert.*;
 
 @SpringBootTest
 class NotebookApplicationTests {
 
-	@Test
-	void contextLoads() {
+	@Autowired
+	NoteController noteController;
+
+	void setupClient() {
+		when(noteController.createMessage(any(Note.class)))
+				.thenReturn(Mono.empty());
 	}
 
+	@Test
+	void testCreateMessage() {
+		setupClient();
+
+		var response = noteController.createMessage(new Note(1, "this is a test note"));
+
+		assertNotNull(response);
+	}
+	
 }
